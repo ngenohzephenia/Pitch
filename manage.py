d784696c484d692fc5a8a562d9b6193a1fc26661
ngenohzephenia <ngenohzef@gmail.com>
2021-03-03 00:34:11 +0300

    added a decorator

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
-
+from app.models import User
 #...
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
 if __name__ == '__main__':
     manager.run()
